0d566ca36a25db45f1c6f966b0fcb939e4fea513
MaxBenen <max_benen@hotmail.com>
2023-05-16 17:46:30 +0200

    added animation to intro

--- javascript.js
+++ javascript.js
@@ -2,3 +2,26 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header').classList.toggle('active');
   });
+  
+  const introElement = document.querySelector('.intro');
+  const introNameElement = document.querySelector('.intro-name');
+  const text = introElement.textContent.trim();
+  let charIndex = 0;
+  
+  function type() {
+    introElement.textContent = text.slice(0, charIndex + 1);
+  
+    if (charIndex < text.length - 1) {
+      charIndex++;
+      introNameElement.style.color = 'aquamarine'; // Apply the desired styling to .intro-name
+      setTimeout(type, 200); // Delay between each character typing
+    } else {
+      introNameElement.style.color = 'aquamarine'; // Apply the desired styling to .intro-name
+    }
+  }
+  
+  type();
+  
+  
+  
+  
